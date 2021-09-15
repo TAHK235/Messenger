7beb8652cfcddef05b06213e4df93f0a279f8f67
Hawkeneyes <mr.lomomakin@mail.ru>
2021-09-15 23:41:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Messenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
