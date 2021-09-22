977ec61f45f9719593f49875d87ad94851782f6a
Hawkeneyes <mr.lomomakin@mail.ru>
2021-09-22 22:38:33 +0300

    add server add WPF support

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
 
 namespace Messenger
 {
     class Program
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessengerClientAPI API = new MessengerClientAPI();
+
+        private static void GetNewMessage()
+        {
+            Message msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Message msg = new Message("%Username%", "Hi", DateTime.UtcNow);
+            //string output = JsonConvert.SerializeObject(msg);
+            //Console.WriteLine(output);
+            //Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            //Console.WriteLine(deserializedMsg);
+            //{ "UserName":"%Username%","MessageText":"Hi","TimeStamp":"2021-09-22T10:37:58.0525037Z"}
+            //%Username% < 22.09.21 10:37:58 >: Hi
+            MessageID = 1;
+            Console.WriteLine("Введите Ваше имя: ");
+            //UserName = "%UserName%";
+            UserName = Console.ReadLine();
+            string MessageText = "";
+            while (MessageText != "exit")
+            {
+                GetNewMessage();
+                MessageText = Console.ReadLine();
+                if (MessageText.Length > 1)
+                {
+                    Message Sendmsg = new Message(UserName, MessageText, DateTime.Now);
+                    API.SendMessage(Sendmsg);
+                }
+            }
         }
     }
 }
---
+++ Server/Message.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger
+{
+    [Serializable]
+    public class Message
+    {
+        public string UserName { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public Message(string userName, string messageText, DateTime timeStamp)
+        {
+            UserName = userName;
+            MessageText = messageText;
+            TimeStamp = timeStamp;
+        }
+        public Message()
+        {
+            UserName = "System";
+            MessageText = "Server is running...";
+            TimeStamp = DateTime.UtcNow;
+        }
+
+        public override string ToString()
+        {
+            string output = String.Format("{0} <{2}>: {1}", UserName, MessageText, TimeStamp);
+            return output;
+        }
+    }
+}
